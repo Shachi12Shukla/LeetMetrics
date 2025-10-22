fcebbc3cee16c1561625b9c941ccfd925e9bc0e7
Shachi Shukla <shachi.03shukla@gmail.com>
2025-10-22 22:05:29 +0530

    added imp comment about graphql body

--- script.js
+++ script.js
@@ -42,7 +42,8 @@ document.addEventListener("DOMContentLoaded" , function() {
             const targeturl = `https://leetcode.com/graphql/`;
             const myHeaders = new Headers();
             myHeaders.append("content-type", "application/JSON");
-
+             
+            // COPY the graphql from  the "headers" section in the respective graphql query of the "response" row on the network section of the browser page.
             const graphql = JSON.stringify( {
                 query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    " ,
                 variables: {"username": `${username}`} 
